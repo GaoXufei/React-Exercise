bfeefdda3c83b38f41303a161c9003c50adcc1a7
gaoxufei <18047170938@163.com>
2019-12-30 22:22:24 +0800

    transition

--- src/layouts/default.tsx
+++ src/layouts/default.tsx
@@ -2,6 +2,7 @@ import styled from 'styled-components';
 
 export const LayoutWrapper: any = styled.div`
   width: 1200px;
+  min-height: 100%;
   margin: auto;
   overflow: ${ (props: any) => props.overflow || 'auto'};
 `;
